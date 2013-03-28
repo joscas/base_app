425dc3dee3d39c4016e525e6092ad1ea2d8c4fec
iris <joscas@gmail.com>
2013-03-28 23:40:36 +0100

    tweak for callback3 decodeURIComponent

--- app/assets/javascripts/routes/callbackback.js
+++ app/assets/javascripts/routes/callbackback.js
@@ -1,6 +1,6 @@
 BaseApp.CallbackRoute = Ember.Route.extend({
   setupController: function(controller, params) {
-    var token = params.remember_token;
+    var token = decodeURIComponent(params.remember_token); //Token comes encoded
     $.cookie('ember-auth-remember-me', token, {expires: Auth.Config.get('rememberPeriod'), path: '/'});
     this.transitionTo('home');
   },
--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -14,7 +14,8 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @user.persisted?
       @user.ensure_authentication_token!
       @user.remember_me!
-      url_token=remember_token(@user).html_safe
+      # Encode so that we can decode with JS decodeURIComponent in the browser
+      url_token=URI.escape(remember_token(@user), Regexp.new("[^#{URI::PATTERN::UNRESERVED}]")) 
 	    redirect_to "/callback/#{url_token}" 
     else
       redirect_to "/err_500"
