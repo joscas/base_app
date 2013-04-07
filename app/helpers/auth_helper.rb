0e463d93b328e9988f313a02b39ecb68664492b1
Josep Casals <joscas@gmail.com>
2013-04-08 00:09:19 +0200

    Tests for sessions controller

--- app/helpers/auth_helper.rb
+++ app/helpers/auth_helper.rb
@@ -8,9 +8,10 @@ module AuthHelper
   end
   
   def admin_only!
-    auth_only!
-    unless current_user.admin?
-      render json: {}, status: 401
+    unless params[:auth_token] && user_signed_in?
+      unless current_user && current_user.admin?
+        render json: {}, status: 401
+      end
     end
   end
 end
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe UsersController do
+  let(:user) { Fabricate(:user) }
+  before { user } # initialize it
+
+  describe 'GET index' do
+    context 'unauthorized' do
+      before { get :index }
+
+      it 'returns http 401' do
+        response.response_code.should == 401
+      end
+    end
+
+    context 'authorized' do
+      before do
+        user.ensure_authentication_token!
+        get :index, auth_token: user.authentication_token
+      end
+
+      it 'returns http 200' do
+        response.response_code.should == 200
+      end
+    end
+  end
+
+  describe 'GET show' do
+    context 'unauthorized' do
+      before { get :show, id: user.id }
+
+      it 'returns http 401' do
+        response.response_code.should == 401
+      end
+    end
+
+    context 'authorized' do
+      before do
+        user.ensure_authentication_token!
+        get :show, id: user.id, auth_token: user.authentication_token
+      end
+
+      it 'returns http 200' do
+        response.response_code.should == 200
+      end
+    end
+  end
+end
