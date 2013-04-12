6bdce41aeacae3e155814cfa22d2d526e534684f
Josep Casals <joscas@gmail.com>
2013-04-12 19:58:35 +0200

    Asset precompile

--- features/support/env.rb
+++ features/support/env.rb
@@ -12,8 +12,8 @@ SimpleCov.start 'rails'
 
 require 'cucumber/rails'
 
-#require 'capybara/poltergeist'
-#Capybara.javascript_driver = :poltergeist
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
 
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
