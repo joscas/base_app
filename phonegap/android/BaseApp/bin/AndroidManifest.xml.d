67b9cc2c6bd27c48b497fa91407bc7b37d2e2618
Josep Casals <joscas@gmail.com>
2013-05-03 23:39:44 +0200

    scale image

--- phonegap/android/BaseApp/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/joscas/projects2/base_app/phonegap/android/BaseApp/bin/AndroidManifest.xml : \
-/Users/joscas/projects2/base_app/phonegap/android/BaseApp/AndroidManifest.xml \
--- phonegap/android/BaseApp/bin/BaseApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/joscas/projects2/base_app/phonegap/android/BaseApp/bin/BaseApp-debug-unaligned.apk : \
-/Users/joscas/projects2/base_app/phonegap/android/BaseApp/bin/BaseApp.ap_ \
-/Users/joscas/projects2/base_app/phonegap/android/BaseApp/bin/classes.dex \
-/Users/joscas/projects2/base_app/phonegap/android/BaseApp/libs/cordova-2.6.0.jar \
